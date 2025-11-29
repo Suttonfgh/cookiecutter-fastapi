2d193dd2d55838500a006606dca1f104698812df
Suttonfgh <sultanyessengeldi@gmail.com>
2025-11-30 02:05:02 +0500

    test: rename fields for Avelio

--- {{cookiecutter.project_slug}}/app/models/log.py
+++ {{cookiecutter.project_slug}}/app/models/log.py
@@ -7,5 +7,4 @@ class RequestLog(Base):
     __tablename__ = "request_logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    request = Column(Text, nullable=False)
     response = Column(Text, nullable=False)
